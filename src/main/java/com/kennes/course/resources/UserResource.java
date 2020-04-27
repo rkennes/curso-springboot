8d37c3f404a7676e03c8e88363319de2ff16b071
rkennes <rodrigo.kennes@gmail.com>
2020-04-27 11:12:09 -0300

    UserResources

---
+++ src/main/java/com/kennes/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.kennes.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kennes.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Kennes", "Kennes@gmail.com", "519985191", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+}
